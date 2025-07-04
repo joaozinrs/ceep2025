36eb5ff86a53928a7a7dc13c6e9d21980554fd9f
joaozinrs <santos.silva.joao2710@escola.pr.gov.br>
2025-07-04 09:41:39 -0300

    calculadora de idade

---
+++ calculando idade/# Calculando a idade de uma pessoa.py
@@ -0,0 +1,9 @@
+# calculando a idade
+from datetime import date
+
+ano_nascimento = int(input("Digite seu ano de nascimento: "))
+ano_atual = date.today().year
+
+idade = ano_atual - ano_nascimento
+
+print(f"Você tem {idade} anos.")
